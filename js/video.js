8c879ed4215160c0b00d8eb38c6c740cc721ff9b
mdshahinahmed <smshahinahmed4115@gmail.com>
2025-07-24 15:33:13 +0600

    button create

--- js/video.js
+++ js/video.js
@@ -1,6 +1,20 @@
-const createCategories = () => {
+// loadCategories
+const loadCategories = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then()
-    .catch()
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.error(error));
 }
-createCategories();
+// displayCategories categories
+const displayCategories = (categories) => {
+    const buttonContainer = document.getElementById("button-container");
+categories.forEach((item) => {
+    console.log(item);
+    // create a button
+    const button = document.createElement("button");
+    button.classList= "btn";
+    button.innerText = item.category;
+    buttonContainer.append(button);
+})
+}
+loadCategories();
