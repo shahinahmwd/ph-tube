06ab3243756ec5e8db87a063289676cac7d67d22
mdshahinahmed <smshahinahmed4115@gmail.com>
2025-07-25 08:39:57 +0600

    project done

--- js/video.js
+++ js/video.js
@@ -1,52 +1,215 @@
-// loadCategories
-const loadCategories = () => {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-        .then((res) => res.json())
-        .then((data) => displayCategories(data.categories))
-        .catch((error) => console.error(error));
-}
-const loadVideo = () => {
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
-        .then((res) => res.json())
-        .then((data) => videos(data.videos))
-        .catch((error) => console.log(error));
+console.log("index is connected");
+const showLoader = () => {
+  document.getElementById("loader").classList.remove("hidden");
+  document.getElementById("video-container").classList.add("hidden");
+};
+const hideLoader = () => {
+  document.getElementById("loader").classList.add("hidden");
+  document.getElementById("video-container").classList.remove("hidden");
+};
+function removeActiveClass() {
+  const activeButtons = document.getElementsByClassName("active");
+  for (let btn of activeButtons) {
+    btn.classList.remove("active");
+  }
 }
 
-const videos = (videos) => {
-    const videoContainer = document.getElementById("videos")
-    videos.forEach((video) => {
-        console.log(video);
-        // create card
-        const card = document.createElement("div");
-        card.classList = "card card-compact"
-        card.innerHTML = `
-          <figure>
+function loadCategories() {
+  // 1- fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    //2 - convert promise to json
+    .then((res) => res.json())
+    //3 - send data to display
+    .then((data) => displayCategories(data.categories));
+}
+
+function loadVideos(searchText = "") {
+  showLoader();
+  fetch(
+    `https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      removeActiveClass();
+      document.getElementById("btn-all").classList.add("active");
+      displayVideos(data.videos);
+    });
+}
+//1001
+const loadCategoryVideos = (id) => {
+  showLoader();
+  const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+  console.log(url);
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      removeActiveClass();
+      //no active class
+
+      const clickedButton = document.getElementById(`btn-${id}`);
+      clickedButton.classList.add("active");
+
+      displayVideos(data.category);
+    });
+};
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayVideoDetails(data.video));
+};
+
+const displayVideoDetails = (video) => {
+  console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+
+  detailsContainer.innerHTML = `
+   <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
     <img
-      src=${video.thumbnail}
+      src="${video.thumbnail}"
       alt="Shoes" />
   </figure>
   <div class="card-body">
-    <h2 class="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 class="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
     <div class="card-actions justify-end">
-      <button class="btn btn-primary">Buy Now</button>
+      
     </div>
   </div>
-        `
-        videoContainer.append(card);
-    })
+</div>
+  `;
+};
+
+// {
+//     "category_id": "1001",
+//     "category": "Music"
+// }
+function displayCategories(categories) {
+  // get the container
+  const categoryContainer = document.getElementById("category-container");
+
+  // Loop operation on Array of object
+  for (let cat of categories) {
+    // console.log(cat);
+
+    // create Element
+    const categoryDiv = document.createElement("div");
+
+    categoryDiv.innerHTML = `
+    <button id="btn-${cat.category_id}"  onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D]  hover:text-white">${cat.category}</button>
+    `;
+
+    // Append the Element
+    categoryContainer.append(categoryDiv);
+  }
 }
-// displayCategories categories
-const displayCategories = (categories) => {
-    const buttonContainer = document.getElementById("button-container");
-    categories.forEach((item) => {
-        console.log(item);
-        // create a button
-        const button = document.createElement("button");
-        button.classList = "btn";
-        button.innerText = item.category;
-        buttonContainer.append(button);
-    })
-}
+
+// {
+//     "category_id": "1003",
+//     "video_id": "aaak",
+//     "thumbnail": "https://i.ibb.co/ZNggzdm/cake.jpg",
+//     "title": "Beyond The Pale",
+//     "authors": [
+//         {
+//             "profile_picture": "https://i.ibb.co/MZ2vbXR/jimm.jpg",
+//             "profile_name": "Jim Gaffigan",
+//             "verified": false
+//         }
+//     ],
+//     "others": {
+//         "views": "2.6K",
+//         "posted_date": "15400"
+//     },
+//     "description": "'Beyond The Pale' by Jim Gaffigan, with 2.6K views, is a comedic gem that explores everyday observations and family life with a light-hearted and witty approach. Jim's humor is accessible and delightful, making this show perfect for anyone who enjoys clean, observational comedy."
+// }
+
+const displayVideos = (videos) => {
+  const videoContainer = document.getElementById("video-container");
+
+  videoContainer.innerHTML = "";
+
+  if (videos.length == 0) {
+    videoContainer.innerHTML = `
+    <div
+        class="py-20 col-span-full flex flex-col justify-center items-center text-center"
+      >
+        <img class="w-[120px]" src="./assets/Icon.png" alt="" />
+        <h2 class="text-2xl font-bold">
+          Oops!! Sorry, There is no content here
+        </h2>
+      </div>
+    `;
+    hideLoader();
+    return;
+  }
+
+  videos.forEach((video) => {
+    // console.log(video);
+
+    const videoCard = document.createElement("div");
+
+    videoCard.innerHTML = `
+     <div class="card bg-base-100">
+        <figure class="relative">
+          <img class="w-full h-[150px] object-cover" src="${
+            video.thumbnail
+          }" alt="Shoes" />
+          <span
+            class="absolute bottom-2 right-2 text-sm rounded text-white bg-black px-2"
+            >3hrs 56 min ago</span
+          >
+        </figure>
+
+        <div class="flex gap-3 px-0 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div
+                class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2"
+              >
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div class="intro">
+            <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+            <p class="text-sm text-gray-400 flex gap-1">
+             ${video.authors[0].profile_name}
+              ${
+                video.authors[0].verified == true
+                  ? `<img
+                class="w-5 h-5"
+                src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png"
+                alt=""
+              />`
+                  : ``
+              }
+            </p>
+            <p class="text-sm text-gray-400">${video.others.views} views</p>
+          </div>
+
+        </div>
+        <button onclick=loadVideoDetails('${
+          video.video_id
+        }') class="btn btn-block">Show Details</button>
+      </div>
+    
+    `;
+    //append
+    videoContainer.append(videoCard);
+  });
+  hideLoader();
+};
+
+document.getElementById("search-input").addEventListener("keyup", (e) => {
+  const input = e.target.value;
+  loadVideos(input);
+});
+
 loadCategories();
-loadVideo()
